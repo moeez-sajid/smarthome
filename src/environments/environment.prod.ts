c6d109263cde207df9dc1d83b3b49edb29825d27
Moeez Sajid <moeez.sajid@gagamuller.com>
2025-04-06 16:21:57 +0100

    env files

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://api.yourblogdomain.com/api',
+  baseUrl: 'https://www.yourblogdomain.com',
+  siteName: 'SmartHome Blog',
+  siteDescription: 'Your source for smart home technology and automation',
+  defaultImage: 'https://yourdomain.com/images/default-blog-image.jpg'
+}; 
---
+++ src/environments/environment.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: false,
+  apiUrl: 'http://localhost:3000/api',
+  baseUrl: 'http://localhost:4200',
+  siteName: 'SmartHome Blog',
+  siteDescription: 'Your source for smart home technology and automation',
+  defaultImage: 'https://yourdomain.com/images/default-blog-image.jpg'
+}; 
